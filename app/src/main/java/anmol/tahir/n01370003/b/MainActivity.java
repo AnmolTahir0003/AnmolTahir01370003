752b14299aa7cb11a3e203079bdcc0734afcfb77
Anmol Tahir <anmoltahir001@gmail.com>
2022-04-22 18:08:25 -0400

    Initial commit: Project created

---
+++ app/src/main/java/anmol/tahir/n01370003/b/MainActivity.java
@@ -0,0 +1,14 @@
+package anmol.tahir.n01370003.b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
